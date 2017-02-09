68b52a9f4edd83133ca615117b7369f562ce5a0e
Jakub Fraczek <kuba.fraczek@gmail.com>
2017-02-09 18:10:47 +0100

    3rd valid version, still unaccepted bu spoj

--- src/main/java/spoj/PrimeGenerator.java
+++ src/main/java/spoj/PrimeGenerator.java
@@ -11,23 +11,55 @@ public class PrimeGenerator {
 
 		System.out.println("liczba linii");
 		int a = sc.nextInt();
+
+		List<Integer> primesList = new ArrayList<Integer>();
+		boolean[] primesArray = new boolean[10000000];
+
+		for (int i = 2; i < primesArray.length; i++) {
+			primesArray[i] = true;
+		}
+		int max = 10000000;
+		double n = Math.sqrt(max);
+		
+		for (int i = 2; i < n; i++) {
+			for (int j = i * i; j < max; j += i) {
+				primesArray[j] = false;
+			}
+		}
+		
+		for (int i = 31623; i < 10000000; i++){
+			primesArray[i] = false;
+		}
+
+		for (int i = 2; i < primesArray.length; i++) {
+			if(primesArray[i]){
+				primesList.add(i);
+			}
+		}
+//		int index = 0;
+//		for(int p : primesList){
+//			System.out.println("Index: " + index + ", value: " + p);
+//			index++;
+//		}
+		
 		for (int i = 0; i < a; i++) {
 			System.out.println("zakres");
 			int b = sc.nextInt();
 			int c = sc.nextInt();
-			for (int j = b; j <= c; j++) {
-				List<Integer> pierwsze = new ArrayList<Integer>();
-				for (int x = 1; x <= j; x++) {
-					if (j % x == 0) {
-						pierwsze.add(j);
-					}
-				}
-				if (pierwsze.size() == 2) {
-					System.out.println(j);
+			if (b<2){
+				b = 2;
+			}
+			for (int j = b; j <= c; j++){
+				int dupa = 0;
+				if(primesList.contains(j)){
+					dupa = primesList.indexOf(j);
+					System.out.println(primesList.get(dupa));
 				}
 			}
+
 		}
 
+		sc.close();
 	}
 
 }
