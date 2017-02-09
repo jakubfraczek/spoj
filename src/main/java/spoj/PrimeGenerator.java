2e1518f937f450908548acf086d260cea3e1dcf1
Jakub Fraczek <kuba.fraczek@gmail.com>
2017-02-09 12:12:53 +0100

    working prototype - not accepted by spoj

--- src/main/java/spoj/PrimeGenerator.java
+++ src/main/java/spoj/PrimeGenerator.java
@@ -10,21 +10,54 @@ public class PrimeGenerator {
 			String line = buf.readLine();
 			int lines = Integer.parseInt(line);
 
-			for (int i = 0; i < lines; i++) {
+			for (int index = 0; index < lines; index++) {
+
+				int[] tab = null;
+
 				line = buf.readLine();
 				String[] dupa = line.split(" ", 2);
 				int a = Integer.parseInt(dupa[0]);
-				int b = Integer.parseInt(dupa[1]);
-				if (a <= 2){
-					//http://www.algorytm.edu.pl/algorytmy-maturalne/sito-eratostenesa.html
-//					https://pl.wikipedia.org/wiki/Sito_Eratostenesa
+				int n = Integer.parseInt(dupa[1]);
+
+				if (a <= 2) {
+					a = 2;
+					tab = findPrime(n);
+
+				} else {
+					tab = findPrime(n);
 				}
+
+				for (int t : tab) {
+					if (t >= a && t != 0) {
+						System.out.println(t);
+					}
+				}
+
 			}
 
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 
+	}
+
+	private static int[] findPrime(int n) {
+		int[] tab;
+		tab = new int[n + 1];
+		for (int i = 0; i < tab.length; i++) {
+			tab[i] = i;
+		}
+
+		for (int i = 2; i * i < n; i++) {
+			if (tab[i] != 0) {
+				int j = i + i;
+				while (j < n) {
+					tab[j] = 0;
+					j += i;
+				}
+			}
+		}
+		return tab;
 	}
 
 }
